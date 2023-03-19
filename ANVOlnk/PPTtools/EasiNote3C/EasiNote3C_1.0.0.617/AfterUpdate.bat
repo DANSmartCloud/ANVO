95f40b96579f162dc0164920055cfeba959835bd
DANSmartCloud <DANzhiyun2020@foxmail.com>
2023-03-19 12:11:16 +0800

    6

---
+++ ANVOlnk/PPTtools/EasiNote3C/EasiNote3C_1.0.0.617/AfterUpdate.bat
@@ -0,0 +1,5 @@
+cd "%~dp0"
+%~d0
+
+"Main\SupportFiles\K-Lite_Codec_Pack_1196_Basic.exe" /verysilent /norestart /LoadInf=".\Main\SupportFiles\klcp_basic_unattended.ini"
+Main\SupportFiles\EasiUpdateSetup.exe /S
